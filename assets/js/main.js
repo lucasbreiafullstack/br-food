6a18e994c0b6caf6a00c53564d33d9b1615bc737
unknown <lucas.devfullstack021@gmail.com>
2023-09-25 16:20:09 -0300

    trabalhando na nav

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop:'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/*=============== DEALS TAB ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
